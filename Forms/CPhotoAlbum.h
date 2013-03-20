e1310c8c8b7b8f4a665f6e1ea01fa2b414e5b0ad
skynowa <skynowa@gmail.com>
2013-03-20 21:35:11 +0200

    REVIEW: code style

--- Forms/CPhotoAlbum.h
+++ Forms/CPhotoAlbum.h
@@ -20,59 +20,57 @@ class CPhotoAlbum :
     Q_OBJECT
 
 public:
-    Ui::CPhotoAlbum    m_Ui;
+    Ui::CPhotoAlbum m_Ui;
 
-                       CPhotoAlbum        (QWidget *parent,
-                                           QSqlTableModel *tableModel,
-                                           CSqlNavigator *sqlNavigator);
-    virtual           ~CPhotoAlbum        ();
+                    CPhotoAlbum         (QWidget *parent,
+                                         QSqlTableModel *tableModel,
+                                         CSqlNavigator *sqlNavigator);
+    virtual        ~CPhotoAlbum         ();
 
 protected:
-    virtual bool       eventFilter        (QObject *obj, QEvent *event);
-    virtual void       showEvent          (QShowEvent *event);
-    virtual void       resizeEvent        (QResizeEvent *event);
-    virtual void       closeEvent         (QCloseEvent *event);
+    virtual bool    eventFilter         (QObject *obj, QEvent *event);
+    virtual void    showEvent           (QShowEvent *event);
+    virtual void    resizeEvent         (QResizeEvent *event);
+    virtual void    closeEvent          (QCloseEvent *event);
 
 private:
-    QSqlTableModel    *_m_tmModel;
-    CSqlNavigator     *_m_snSqlNavigator;
-    cint               _m_ciDbRecordIndex;
-    db_images_t        _m_viDbItems;
-    QPixmap            _m_pixPhoto;
+    QSqlTableModel *_m_tmModel;
+    CSqlNavigator  *_m_snSqlNavigator;
+    cint            _m_ciDbRecordIndex;
+    db_images_t     _m_viDbItems;
+    QPixmap         _m_pixPhoto;
 
-    void               _construct         ();
-    void               _destruct          ();
-    void               _initMain          ();
-    void               _initActions       ();
+    void            _construct          ();
+    void            _destruct           ();
+    void            _initMain           ();
+    void            _initActions        ();
 
-    void               _photoUpdate       (const QPixmap &pixmap);
+    void            _photoUpdate        (const QPixmap &pixmap);
 
 private Q_SLOTS:
     // group "Exit"
-    void               slot_OnExit        ();
+    void            slot_OnExit         ();
 
     // group "Edit"
-    void               slot_OnSaveAs      ();
-    void               slot_OnFirst       ();
-    void               slot_OnPrior       ();
-    void               slot_OnNext        ();
-    void               slot_OnLast        ();
-    void               slot_OnGoTo        ();
-    void               slot_OnInsert      ();
-    void               slot_OnRemove      ();
-    void               slot_OnEdit        ();
-    void               slot_OnSetPrimary  ();
+    void            slot_OnSaveAs       ();
+    void            slot_OnFirst        ();
+    void            slot_OnPrior        ();
+    void            slot_OnNext         ();
+    void            slot_OnLast         ();
+    void            slot_OnGoTo         ();
+    void            slot_OnInsert       ();
+    void            slot_OnRemove       ();
+    void            slot_OnEdit         ();
+    void            slot_OnSetPrimary   ();
 
     // photo
-    void               slot_OnLoop        ();
-    void               slot_OnPhotoUpdate (cint &index);
-    void               slot_photoMini_OnClicked(QLabel *label,
-                                                cQString &dbFieldName);
+    void            slot_OnLoop         ();
+    void            slot_OnPhotoUpdate  (cint &index);
+    void            slot_photoMini_OnClicked(QLabel *label, cQString &dbFieldName);
 
 Q_SIGNALS:
-    void               signal_photo_clicked();
-    void               signal_photoMini_clicked(QLabel *label,
-                                               cQString &dbFieldName);
+    void            signal_photo_clicked();
+    void            signal_photoMini_clicked(QLabel *label, cQString &dbFieldName);
 };
 //------------------------------------------------------------------------------
 #endif // PickupDB_CPhotoAlbumH
