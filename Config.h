5a34002e6919447e1a82c257f734e024433c3a3d
skynowa <skynowa@gmail.com>
2013-03-24 01:02:13 +0200

    ADD: use global SDbRecord

--- Config.h
+++ Config.h
@@ -7,6 +7,8 @@
 #ifndef XXXDb_ConfigH
 #define XXXDb_ConfigH
 //------------------------------------------------------------------------------
+#include <QObject>
+//------------------------------------------------------------------------------
 // application
 #define APP_NAME                    "XXXDb"
 #define APP_ORG                     "Skynowa Studio"
@@ -151,98 +153,202 @@
 *
 *******************************************************************************/
 
-#define DB_CF_ID                    tr("#")
+#define DB_CF_ID                    QObject::tr("#")
 
 // Main
-#define DB_CF_MAIN_NICK             tr("Nick")
-#define DB_CF_MAIN_SURNAME          tr("Surname")
-#define DB_CF_MAIN_NAME             tr("Name")
-#define DB_CF_MAIN_PATRONYMIC       tr("Patronymic")
-#define DB_CF_MAIN_AGE              tr("Age")
-#define DB_CF_MAIN_HEIGHT           tr("Height")
-#define DB_CF_MAIN_WEIGHT           tr("Weight")
-#define DB_CF_MAIN_HAIRLENGTH       tr("Hair length")
-#define DB_CF_MAIN_HAIRCOLOR        tr("Hair color")
-#define DB_CF_MAIN_APPEARANCE       tr("Appearance")
-#define DB_CF_MAIN_STATUS           tr("Status")
+#define DB_CF_MAIN_NICK             QObject::tr("Nick")
+#define DB_CF_MAIN_SURNAME          QObject::tr("Surname")
+#define DB_CF_MAIN_NAME             QObject::tr("Name")
+#define DB_CF_MAIN_PATRONYMIC       QObject::tr("Patronymic")
+#define DB_CF_MAIN_AGE              QObject::tr("Age")
+#define DB_CF_MAIN_HEIGHT           QObject::tr("Height")
+#define DB_CF_MAIN_WEIGHT           QObject::tr("Weight")
+#define DB_CF_MAIN_HAIRLENGTH       QObject::tr("Hair length")
+#define DB_CF_MAIN_HAIRCOLOR        QObject::tr("Hair color")
+#define DB_CF_MAIN_APPEARANCE       QObject::tr("Appearance")
+#define DB_CF_MAIN_STATUS           QObject::tr("Status")
 
 // Phones
-#define DB_CF_PHONES_MOBILE1        tr("Mobile 1")
-#define DB_CF_PHONES_MOBILE2        tr("Mobile 2")
-#define DB_CF_PHONES_MOBILE3        tr("Mobile 3")
-#define DB_CF_PHONES_HOME           tr("Phone home")
-#define DB_CF_PHONES_JOB            tr("Phone job")
-#define DB_CF_PHONES_OTHER          tr("Phone other")
+#define DB_CF_PHONES_MOBILE1        QObject::tr("Mobile 1")
+#define DB_CF_PHONES_MOBILE2        QObject::tr("Mobile 2")
+#define DB_CF_PHONES_MOBILE3        QObject::tr("Mobile 3")
+#define DB_CF_PHONES_HOME           QObject::tr("Phone home")
+#define DB_CF_PHONES_JOB            QObject::tr("Phone job")
+#define DB_CF_PHONES_OTHER          QObject::tr("Phone other")
 
 // Address
-#define DB_CF_ADDRESS_COUNTRY       tr("Country")
-#define DB_CF_ADDRESS_CODE          tr("Code")
-#define DB_CF_ADDRESS_CITY          tr("City")
-#define DB_CF_ADDRESS_DISTRICT      tr("District")
-#define DB_CF_ADDRESS_STREET        tr("Street")
-#define DB_CF_ADDRESS_HOUSE         tr("House")
-#define DB_CF_ADDRESS_PORCH         tr("Porch")
-#define DB_CF_ADDRESS_FLOOR         tr("Floor")
-#define DB_CF_ADDRESS_APARTMENT     tr("Apartment")
+#define DB_CF_ADDRESS_COUNTRY       QObject::tr("Country")
+#define DB_CF_ADDRESS_CODE          QObject::tr("Code")
+#define DB_CF_ADDRESS_CITY          QObject::tr("City")
+#define DB_CF_ADDRESS_DISTRICT      QObject::tr("District")
+#define DB_CF_ADDRESS_STREET        QObject::tr("Street")
+#define DB_CF_ADDRESS_HOUSE         QObject::tr("House")
+#define DB_CF_ADDRESS_PORCH         QObject::tr("Porch")
+#define DB_CF_ADDRESS_FLOOR         QObject::tr("Floor")
+#define DB_CF_ADDRESS_APARTMENT     QObject::tr("Apartment")
 
 // E-mail
-#define DB_CF_EMAIL_EMAIL           tr("E-mail")
+#define DB_CF_EMAIL_EMAIL           QObject::tr("E-mail")
 
 // Web
-#define DB_CF_WEB_WEB               tr("Web")
+#define DB_CF_WEB_WEB               QObject::tr("Web")
 
 // Messengers
-#define DB_CF_MESSENGERS_ICQ        tr("ICQ")
-#define DB_CF_MESSENGERS_SKYPE      tr("Skype")
+#define DB_CF_MESSENGERS_ICQ        QObject::tr("ICQ")
+#define DB_CF_MESSENGERS_SKYPE      QObject::tr("Skype")
 
 // Job
-#define DB_CF_JOB_PROFESSION        tr("Profession")
-#define DB_CF_JOB_ADDRESS           tr("Address")
-#define DB_CF_JOB_SALARY            tr("Salary")
-#define DB_CF_JOB_COMPANY           tr("Company")
+#define DB_CF_JOB_PROFESSION        QObject::tr("Profession")
+#define DB_CF_JOB_ADDRESS           QObject::tr("Address")
+#define DB_CF_JOB_SALARY            QObject::tr("Salary")
+#define DB_CF_JOB_COMPANY           QObject::tr("Company")
 
 // Dates
-#define DB_CF_DATES_BIRTHDAY        tr("Birthday")
-#define DB_CF_DATES_BIRTHDAYFATHER  tr("Birthday father")
-#define DB_CF_DATES_BIRTHDAYMOTHER  tr("Birthday mother")
-#define DB_CF_DATES_DAYDATING       tr("Day of dating")
-#define DB_CF_DATES_DAYFIRSTKISS    tr("Day of 1-st kiss")
-#define DB_CF_DATES_DAYFIRSTSEX     tr("Day of 1-st sex")
+#define DB_CF_DATES_BIRTHDAY        QObject::tr("Birthday")
+#define DB_CF_DATES_BIRTHDAYFATHER  QObject::tr("Birthday father")
+#define DB_CF_DATES_BIRTHDAYMOTHER  QObject::tr("Birthday mother")
+#define DB_CF_DATES_DAYDATING       QObject::tr("Day of dating")
+#define DB_CF_DATES_DAYFIRSTKISS    QObject::tr("Day of 1-st kiss")
+#define DB_CF_DATES_DAYFIRSTSEX     QObject::tr("Day of 1-st sex")
 
 // Interests
-#define DB_CF_INTERESTS_HOBBY       tr("Hobby")
-#define DB_CF_INTERESTS_SPORTS      tr("Sports")
-#define DB_CF_INTERESTS_SMOKING     tr("Smoking")
-#define DB_CF_INTERESTS_ALCOHOL     tr("Alcohol")
-#define DB_CF_INTERESTS_DRUGS       tr("Drugs")
+#define DB_CF_INTERESTS_HOBBY       QObject::tr("Hobby")
+#define DB_CF_INTERESTS_SPORTS      QObject::tr("Sports")
+#define DB_CF_INTERESTS_SMOKING     QObject::tr("Smoking")
+#define DB_CF_INTERESTS_ALCOHOL     QObject::tr("Alcohol")
+#define DB_CF_INTERESTS_DRUGS       QObject::tr("Drugs")
 
 // TODO: Periods
 
 // Etc
-#define DB_CF_ETC_DATECREATION      tr("Date creation")
-#define DB_CF_ETC_DATELASTCHANGE    tr("Date last change")
+#define DB_CF_ETC_DATECREATION      QObject::tr("Date creation")
+#define DB_CF_ETC_DATELASTCHANGE    QObject::tr("Date last change")
 
 // Notes
-#define DB_CF_NOTES_NOTES           tr("Notes")
+#define DB_CF_NOTES_NOTES           QObject::tr("Notes")
 
 // Photos
-#define DB_CF_PHOTOS_1              tr("Photo 1")
-#define DB_CF_PHOTOS_2              tr("Photo 2")
-#define DB_CF_PHOTOS_3              tr("Photo 3")
-#define DB_CF_PHOTOS_4              tr("Photo 4")
-#define DB_CF_PHOTOS_5              tr("Photo 5")
-#define DB_CF_PHOTOS_6              tr("Photo 6")
-#define DB_CF_PHOTOS_7              tr("Photo 7")
-#define DB_CF_PHOTOS_8              tr("Photo 8")
-#define DB_CF_PHOTOS_9              tr("Photo 9")
-#define DB_CF_PHOTOS_10             tr("Photo 10")
-#define DB_CF_PHOTOS_11             tr("Photo 11")
-#define DB_CF_PHOTOS_12             tr("Photo 12")
-#define DB_CF_PHOTOS_13             tr("Photo 13")
-#define DB_CF_PHOTOS_14             tr("Photo 14")
-#define DB_CF_PHOTOS_15             tr("Photo 15")
-#define DB_CF_PHOTOS_PRIMARY        tr("Photo primary")
+#define DB_CF_PHOTOS_1              QObject::tr("Photo 1")
+#define DB_CF_PHOTOS_2              QObject::tr("Photo 2")
+#define DB_CF_PHOTOS_3              QObject::tr("Photo 3")
+#define DB_CF_PHOTOS_4              QObject::tr("Photo 4")
+#define DB_CF_PHOTOS_5              QObject::tr("Photo 5")
+#define DB_CF_PHOTOS_6              QObject::tr("Photo 6")
+#define DB_CF_PHOTOS_7              QObject::tr("Photo 7")
+#define DB_CF_PHOTOS_8              QObject::tr("Photo 8")
+#define DB_CF_PHOTOS_9              QObject::tr("Photo 9")
+#define DB_CF_PHOTOS_10             QObject::tr("Photo 10")
+#define DB_CF_PHOTOS_11             QObject::tr("Photo 11")
+#define DB_CF_PHOTOS_12             QObject::tr("Photo 12")
+#define DB_CF_PHOTOS_13             QObject::tr("Photo 13")
+#define DB_CF_PHOTOS_14             QObject::tr("Photo 14")
+#define DB_CF_PHOTOS_15             QObject::tr("Photo 15")
+#define DB_CF_PHOTOS_PRIMARY        QObject::tr("Photo primary")
 
+
+struct SDbRecord
+    ///< DB fields
+{
+    cint      index;    ///< index
+    cQString  field;    ///< DB field name
+    cQVariant caption;  ///< QTableView header caption
+};
+
+typedef const SDbRecord cSDbRecord;
+
+cSDbRecord g_dbRecords[] = {
+    {0,  DB_F_ID,                   DB_CF_ID},
+
+    // Main
+    {1,  DB_F_MAIN_NICK,            DB_CF_MAIN_NICK},
+    {2,  DB_F_MAIN_SURNAME,         DB_CF_MAIN_SURNAME},
+    {3,  DB_F_MAIN_NAME,            DB_CF_MAIN_NAME},
+    {4,  DB_F_MAIN_PATRONYMIC,      DB_CF_MAIN_PATRONYMIC},
+    {5,  DB_F_MAIN_AGE,             DB_CF_MAIN_AGE},
+    {6,  DB_F_MAIN_HEIGHT,          DB_CF_MAIN_HEIGHT},
+    {7,  DB_F_MAIN_WEIGHT,          DB_CF_MAIN_WEIGHT},
+    {8,  DB_F_MAIN_HAIRLENGTH,      DB_CF_MAIN_HAIRLENGTH},
+    {9,  DB_F_MAIN_HAIRCOLOR,       DB_CF_MAIN_HAIRCOLOR},
+    {10, DB_F_MAIN_APPEARANCE,      DB_CF_MAIN_APPEARANCE},
+    {11, DB_F_MAIN_STATUS,          DB_CF_MAIN_STATUS},
+
+    // Phones
+    {12, DB_F_PHONES_MOBILE1,       DB_CF_PHONES_MOBILE1},
+    {13, DB_F_PHONES_MOBILE2,       DB_CF_PHONES_MOBILE2},
+    {14, DB_F_PHONES_MOBILE3,       DB_CF_PHONES_MOBILE3},
+    {15, DB_F_PHONES_HOME,          DB_CF_PHONES_HOME},
+    {16, DB_F_PHONES_JOB,           DB_CF_PHONES_JOB},
+    {17, DB_F_PHONES_OTHER,         DB_CF_PHONES_OTHER},
+
+    // Address
+    {18, DB_F_ADDRESS_COUNTRY,      DB_CF_ADDRESS_COUNTRY},
+    {19, DB_F_ADDRESS_CODE,         DB_CF_ADDRESS_CODE},
+    {20, DB_F_ADDRESS_CITY,         DB_CF_ADDRESS_CITY},
+    {21, DB_F_ADDRESS_DISTRICT,     DB_CF_ADDRESS_DISTRICT},
+    {22, DB_F_ADDRESS_STREET,       DB_CF_ADDRESS_STREET},
+    {23, DB_F_ADDRESS_HOUSE,        DB_CF_ADDRESS_HOUSE},
+    {24, DB_F_ADDRESS_PORCH,        DB_CF_ADDRESS_PORCH},
+    {25, DB_F_ADDRESS_FLOOR,        DB_CF_ADDRESS_FLOOR},
+    {26, DB_F_ADDRESS_APARTMENT,    DB_CF_ADDRESS_APARTMENT},
+
+    // E-mail
+    {27, DB_F_EMAIL_EMAIL,          DB_CF_EMAIL_EMAIL},
+
+    // Web
+    {28, DB_F_WEB_WEB,              DB_CF_WEB_WEB},
+
+    // Messengers
+    {29, DB_F_MESSENGERS_ICQ,       DB_CF_MESSENGERS_ICQ},
+    {30, DB_F_MESSENGERS_SKYPE,     DB_CF_MESSENGERS_SKYPE},
+
+    // Job
+    {31, DB_F_JOB_PROFESSION,       DB_CF_JOB_PROFESSION},
+    {32, DB_F_JOB_ADDRESS,          DB_CF_JOB_ADDRESS},
+    {33, DB_F_JOB_SALARY,           DB_CF_JOB_SALARY},
+    {34, DB_F_JOB_COMPANY,          DB_CF_JOB_COMPANY},
+
+    // Dates
+    {35, DB_F_DATES_BIRTHDAY,       DB_CF_DATES_BIRTHDAY},
+    {36, DB_F_DATES_BIRTHDAYFATHER, DB_CF_DATES_BIRTHDAYFATHER},
+    {37, DB_F_DATES_BIRTHDAYMOTHER, DB_CF_DATES_BIRTHDAYMOTHER},
+    {38, DB_F_DATES_DAYDATING,      DB_CF_DATES_DAYDATING},
+    {39, DB_F_DATES_DAYFIRSTKISS,   DB_CF_DATES_DAYFIRSTKISS},
+    {40, DB_F_DATES_DAYFIRSTSEX,    DB_CF_DATES_DAYFIRSTSEX},
+
+    // Interests
+    {41, DB_F_INTERESTS_HOBBY,      DB_CF_INTERESTS_HOBBY},
+    {42, DB_F_INTERESTS_SPORTS,     DB_CF_INTERESTS_SPORTS},
+    {43, DB_F_INTERESTS_SMOKING,    DB_CF_INTERESTS_SMOKING},
+    {44, DB_F_INTERESTS_ALCOHOL,    DB_CF_INTERESTS_ALCOHOL},
+    {45, DB_F_INTERESTS_DRUGS,      DB_CF_INTERESTS_DRUGS},
+
+    // TODO: Periods
+
+    // Etc
+    {46, DB_F_ETC_DATECREATION,     DB_CF_ETC_DATECREATION},
+    {47, DB_F_ETC_DATELASTCHANGE,   DB_CF_ETC_DATELASTCHANGE},
+
+    // Notes
+    {48, DB_F_NOTES_NOTES,          DB_CF_NOTES_NOTES},
+
+    // Photos
+    {49, DB_F_PHOTOS_1,             DB_CF_PHOTOS_1},
+    {50, DB_F_PHOTOS_2,             DB_CF_PHOTOS_2},
+    {51, DB_F_PHOTOS_3,             DB_CF_PHOTOS_3},
+    {52, DB_F_PHOTOS_4,             DB_CF_PHOTOS_4},
+    {53, DB_F_PHOTOS_5,             DB_CF_PHOTOS_5},
+    {54, DB_F_PHOTOS_6,             DB_CF_PHOTOS_6},
+    {55, DB_F_PHOTOS_7,             DB_CF_PHOTOS_7},
+    {56, DB_F_PHOTOS_8,             DB_CF_PHOTOS_8},
+    {57, DB_F_PHOTOS_9,             DB_CF_PHOTOS_9},
+    {58, DB_F_PHOTOS_10,            DB_CF_PHOTOS_10},
+    {59, DB_F_PHOTOS_11,            DB_CF_PHOTOS_11},
+    {60, DB_F_PHOTOS_12,            DB_CF_PHOTOS_12},
+    {61, DB_F_PHOTOS_13,            DB_CF_PHOTOS_13},
+    {62, DB_F_PHOTOS_14,            DB_CF_PHOTOS_14},
+    {63, DB_F_PHOTOS_15,            DB_CF_PHOTOS_15},
+    {64, DB_F_PHOTOS_PRIMARY,       DB_CF_PHOTOS_PRIMARY}
+};
 
 /*******************************************************************************
 *   types
--- Forms/CColumns.cpp
+++ Forms/CColumns.cpp
@@ -50,114 +50,17 @@ CColumns::_initMain() {
     }
 
     {
-        struct SHeader {
-            cint      section;
-            cQVariant value;
-        };
-
-        const SHeader chdHeaders[] = {
-            {0,  DB_CF_ID},
-
-            // Main
-            {1,  DB_CF_MAIN_NICK},
-            {2,  DB_CF_MAIN_SURNAME},
-            {3,  DB_CF_MAIN_NAME},
-            {4,  DB_CF_MAIN_PATRONYMIC},
-            {5,  DB_CF_MAIN_AGE},
-            {6,  DB_CF_MAIN_HEIGHT},
-            {7,  DB_CF_MAIN_WEIGHT},
-            {8,  DB_CF_MAIN_HAIRLENGTH},
-            {9,  DB_CF_MAIN_HAIRCOLOR},
-            {10, DB_CF_MAIN_APPEARANCE},
-            {11, DB_CF_MAIN_STATUS},
-
-            // Phones
-            {12, DB_CF_PHONES_MOBILE1},
-            {13, DB_CF_PHONES_MOBILE2},
-            {14, DB_CF_PHONES_MOBILE3},
-            {15, DB_CF_PHONES_HOME},
-            {16, DB_CF_PHONES_JOB},
-            {17, DB_CF_PHONES_OTHER},
-
-            // Address
-            {18, DB_CF_ADDRESS_COUNTRY},
-            {19, DB_CF_ADDRESS_CODE},
-            {20, DB_CF_ADDRESS_CITY},
-            {21, DB_CF_ADDRESS_DISTRICT},
-            {22, DB_CF_ADDRESS_STREET},
-            {23, DB_CF_ADDRESS_HOUSE},
-            {24, DB_CF_ADDRESS_PORCH},
-            {25, DB_CF_ADDRESS_FLOOR},
-            {26, DB_CF_ADDRESS_APARTMENT},
-
-            // E-mail
-            {27, DB_CF_EMAIL_EMAIL},
-
-            // Web
-            {28, DB_CF_WEB_WEB},
-
-            // Messengers
-            {29, DB_CF_MESSENGERS_ICQ},
-            {30, DB_CF_MESSENGERS_SKYPE},
-
-            // Job
-            {31, DB_CF_JOB_PROFESSION},
-            {32, DB_CF_JOB_ADDRESS},
-            {33, DB_CF_JOB_SALARY},
-            {34, DB_CF_JOB_COMPANY},
-
-            // Dates
-            {35, DB_CF_DATES_BIRTHDAY},
-            {36, DB_CF_DATES_BIRTHDAYFATHER},
-            {37, DB_CF_DATES_BIRTHDAYMOTHER},
-            {38, DB_CF_DATES_DAYDATING},
-            {39, DB_CF_DATES_DAYFIRSTKISS},
-            {40, DB_CF_DATES_DAYFIRSTSEX},
-
-            // Interests
-            {41, DB_CF_INTERESTS_HOBBY},
-            {42, DB_CF_INTERESTS_SPORTS},
-            {43, DB_CF_INTERESTS_SMOKING},
-            {44, DB_CF_INTERESTS_ALCOHOL},
-            {45, DB_CF_INTERESTS_DRUGS},
-
-            // TODO: Periods
-
-            // Etc
-            {46, DB_CF_ETC_DATECREATION},
-            {47, DB_CF_ETC_DATELASTCHANGE},
-
-            // Notes
-            {48, DB_CF_NOTES_NOTES},
-
-            // Photos
-            {49, DB_CF_PHOTOS_1},
-            {50, DB_CF_PHOTOS_2},
-            {51, DB_CF_PHOTOS_3},
-            {52, DB_CF_PHOTOS_4},
-            {53, DB_CF_PHOTOS_5},
-            {54, DB_CF_PHOTOS_6},
-            {55, DB_CF_PHOTOS_7},
-            {56, DB_CF_PHOTOS_8},
-            {57, DB_CF_PHOTOS_9},
-            {58, DB_CF_PHOTOS_10},
-            {59, DB_CF_PHOTOS_11},
-            {60, DB_CF_PHOTOS_12},
-            {61, DB_CF_PHOTOS_13},
-            {62, DB_CF_PHOTOS_14},
-            {63, DB_CF_PHOTOS_15},
-            {64, DB_CF_PHOTOS_PRIMARY}
-        };
-
         // set caption for DB fields
+        Q_ASSERT(qARRAY_LENGTH(g_dbRecords) == (size_t)_tmModel->columnCount());
+
         CMain *wnd = static_cast<CMain *>(parent());
 
-        for (int i = 0; i < _tmModel->columnCount(); ++ i) {
+        for (size_t i = 0; i < qARRAY_LENGTH(g_dbRecords); ++ i) {
             cbool isVisible = !wnd->ui.tvInfo->isColumnHidden(i);
 
             QListWidgetItem *item = new QListWidgetItem(ui.lwItems);
             item->setCheckState( isVisible ? Qt::Checked : Qt::Unchecked);
-            item->setText( chdHeaders[i].value.toString() );
+            item->setText( g_dbRecords[i].caption.toString() );
 
             ui.lwItems->addItem(item);
         }
