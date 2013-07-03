80bbfeb63c8241318864e35750cbe4a54f872154
skynowa <skynowa@gmail.com>
2013-07-03 14:34:26 +0300

    UPDATE: CDbImage

--- Widgets/CDbImage/CDbImage.h
+++ Widgets/CDbImage/CDbImage.h
@@ -16,7 +16,7 @@
 class CDbImage;
 
 typedef QVector<CDbImage *> db_images_t;
-typedef const db_images_t        cdb_images_t;
+typedef const db_images_t   cdb_images_t;
 
 class CDbImage :
     public QLabel
@@ -25,76 +25,76 @@ class CDbImage :
 Q_OBJECT
 
 public:
-                          CDbImage(QWidget *parent = NULL);
-                          CDbImage(QWidget *parent, QSqlTableModel *tableModel,
-                                   cQString &dbFieldName, cint &index,
-                                   cint &dbRecordIndex,
-                                   QLabel *label, cQSize &size, QLabel *info);
+                       CDbImage(QWidget *parent = NULL);
+                       CDbImage(QWidget *parent, QSqlTableModel *tableModel,
+                                cQString &dbFieldName, cint &index,
+                                cint &dbRecordIndex,
+                                QLabel *label, cQSize &size, QLabel *info);
         ///< constructor
-    virtual              ~CDbImage();
+    virtual           ~CDbImage();
         ///< destructor
 
     // data
-    cQString &            dbFieldName() const;
+    cQString &         dbFieldName() const;
         ///< get DB field name
-    cint &                index() const;
+    cint &             index() const;
         ///< get index
-    cint &                dbRecordIndex() const;
+    cint &             dbRecordIndex() const;
         ///< get DB record index
-    QLabel *              label() const;
+    QLabel *           label() const;
         ///< get QLabel
-    cQSize &              size() const;
+    cQSize &           size() const;
         ///< get image sizes
-    QLabel *              info() const;
+    QLabel *           info() const;
         ///< get info QLabel
-    QDataWidgetMapper    *mapper();
+    QDataWidgetMapper *mapper();
         ///< get data widget mapper
 
     // actions
-    bool                  isEmpty() const;
+    bool               isEmpty() const;
         ///< is empty image
-    void                  loadFromFile();
+    void               loadFromFile();
         ///< load image from file
-    void                  saveToFile();
+    void               saveToFile();
         ///< save image to file
-    void                  remove();
+    void               remove();
         ///< remove image
 
     // static
-    static int            currentIndex;         ///< get current index
-    static CDbImage *currentDbImageLabel;  ///< get current CDbImage
+    static int         currentIndex;         ///< get current index
+    static CDbImage   *currentDbImageLabel;  ///< get current CDbImage
 
-    static CDbImage *find(cdb_images_t &dbItems, const QLabel *label);
+    static CDbImage   *find(cdb_images_t &dbItems, const QLabel *label);
         ///< find CDbImage by QLabel
-    static CDbImage *find(cdb_images_t &dbItems, cint &index);
+    static CDbImage   *find(cdb_images_t &dbItems, cint &index);
         ///< find CDbImage by index
-    static bool           isEmpty(cdb_images_t &dbItems);
+    static bool        isEmpty(cdb_images_t &dbItems);
         ///< is all QLabels empty
 
 Q_SIGNALS:
-    void                  sig_dataChanged(cint &index);
+    void               sig_dataChanged(cint &index);
 
 private:
-    QWidget              *_wdParent;        ///< parent QWidget
-    QSqlTableModel       *_tmModel;         ///< QSqlTableModel
-    cQString              _csDbFieldName;   ///< DB field name
-    cint                  _ciIndex;         ///< index
-    cint                  _ciDbRecordIndex; ///< DB record index
-    QLabel               *_lblLabel;        ///< QLabel for display image
-    cQSize                _cszSize;         ///< image sizes
-    QLabel               *_lblInfo;         ///< QLabel for display image info
-    QDataWidgetMapper    *_dmMapper;        ///< data widget mapper
-    QByteArray            _baBuffer;        ///< buffer for store image
+    QWidget           *_wdParent;        ///< parent QWidget
+    QSqlTableModel    *_tmModel;         ///< QSqlTableModel
+    cQString           _csDbFieldName;   ///< DB field name
+    cint               _ciIndex;         ///< index
+    cint               _ciDbRecordIndex; ///< DB record index
+    QLabel            *_lblLabel;        ///< QLabel for display image
+    cQSize             _cszSize;         ///< image sizes
+    QLabel            *_lblInfo;         ///< QLabel for display image info
+    QDataWidgetMapper *_dmMapper;        ///< data widget mapper
+    QByteArray         _baBuffer;        ///< buffer for store image
 
-    QDataWidgetMapper    *_map();
+    QDataWidgetMapper *_map();
         ///< map QWidget to DB field
-    void                  _loadFromFile(cQString &filePath);
+    void               _loadFromFile(cQString &filePath);
         ///< load image from file
-    void                  _saveToFile(cQString &filePath);
+    void               _saveToFile(cQString &filePath);
         ///< save image to file
-    void                  _remove();
+    void               _remove();
         ///< remove image
-    void                  _flush();
+    void               _flush();
         ///< flush image buffer
 };
 //------------------------------------------------------------------------------
