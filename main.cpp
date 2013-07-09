b2471fd74b925e4560fb7753ffcc2cabf6ec861e
skynowa <skynowa@gmail.com>
2013-07-09 22:45:54 +0300

    FIX: CApplication::selfCheck()

--- main.cpp
+++ main.cpp
@@ -19,8 +19,10 @@ int main(int argc, char *argv[])
     bool bRv = CApplication::isRunnig(APP_GUID);
     qCHECK_RET(!bRv, EXIT_SUCCESS);
 
+#if 0
     bRv = CApplication::selfCheck();
     qCHECK_RET(!bRv, EXIT_FAILURE);
+#endif
 
     CApplication::setOrganizationName(APP_ORG);
     CApplication::setApplicationName(APP_NAME);
@@ -29,6 +31,14 @@ int main(int argc, char *argv[])
     // start application
     {
         CApplication apApp(argc, argv);
+
+        bRv = QDir( CApplication::dbFilePath() ).isReadable();
+        if (!bRv) {
+            std::wcerr << "Missing database file: "
+                       << CApplication::dbFilePath().toStdWString()
+                       << std::endl;
+            return EXIT_FAILURE;
+        }
 
         CMain wndMain(NULL, 0);
         wndMain.show();
