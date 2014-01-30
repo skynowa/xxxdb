7fece83967bcaf36f0845106d301ac3d8cd3bf4b
skynowa <skynowa@gmail.com>
2014-01-30 17:11:09 +0200

    CIni: fix code style

--- Classes/CIni.h
+++ Classes/CIni.h
@@ -19,36 +19,50 @@ class CIni
     /// application settings
 {
 private:
-                      CIni();
-                     ~CIni();
+               CIni();
+              ~CIni();
 
 public:
-    static void       construct();
-    static void       destruct();
+    static
+    void       construct();
+    static
+    void       destruct();
 
-    static void       get(CMain *wnd);
-    static void       set(CMain *wnd);
+    static
+    void       get(CMain *wnd);
+    static
+    void       set(CMain *wnd);
 
-    static void       get(CAlbum *wnd);
-    static void       set(CAlbum *wnd);
+    static
+    void       get(CAlbum *wnd);
+    static
+    void       set(CAlbum *wnd);
 
 public:
-    static bool       photos_isDeleteFromDisk();
-    static void       setPhotos_isDeleteFromDisk(const bool &value);
+    static
+    bool       photos_isDeleteFromDisk();
+    static
+    void       setPhotos_isDeleteFromDisk(const bool &value);
 
 private:
-    static QSettings *_iniApp;
+    static
+    QSettings *_iniApp;
 
-    static void       _load();
-    static void       _save();
+    static
+    void       _load();
+    static
+    void       _save();
 
     // group "Photos"
-    static bool       _photos_isDeleteFromDisk;  ///< is delete photo from disk after import
+    static
+    bool       _photos_isDeleteFromDisk;  ///< is delete photo from disk after import
 
     template <typename T>
-    static void       _commonGet(T *wnd);
+    static
+    void       _commonGet(T *wnd);
     template <typename T>
-    static void       _commonSet(T *wnd);
+    static
+    void       _commonSet(T *wnd);
 };
 //-------------------------------------------------------------------------------------------------
 #endif // XXXDb_CIniH
